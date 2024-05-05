795bbc5d09fb013bf12e9e23748470ce0b6fff3e
DXNTKEY <purplecs96@gmail.com>
2024-05-05 20:03:30 +0700

    Добавьте файлы проекта.

---
+++ anket/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите ваш возраст: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+       
+        Console.Write("Введите дату вашего рождения (в формате ДД.ММ.ГГГГ): ");
+        DateTime birthDate = Convert.ToDateTime(Console.ReadLine());
+
+        Console.WriteLine("\nИмя: " + name);
+        Console.WriteLine("Возраст: " + age);
+        Console.WriteLine("Дата рождения: " + birthDate.ToShortDateString());
+
+
+
+
+
+
+    }
+}
